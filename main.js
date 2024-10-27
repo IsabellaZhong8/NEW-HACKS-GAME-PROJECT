4de08e4cb1bbd2b8f26bcc6f8ccae1b2f8730fe4
IsabellaZhong <145700170+IsabellaZhong8@users.noreply.github.com>
2024-10-27 00:19:18 +0000

    orders

--- main.js
+++ main.js
@@ -1,13 +1,4 @@
-const myImage = document.querySelector('img');
 
-myImage.onClick = () => {
-    const mySrc = myImage.getAttribute("src");
-    if (mySrc === "https://iili.io/2qSvw7a.md.png") {
-        myImage.setAttribute("src", "https://iili.io/2qSvw7a.md.png");
-    } else {
-        myImage.setAttribute("src", "https://iili.io/2qSr5Rn.md.png");
-    }
-};
 
 class Food {
     constructor(food_name, price) {
@@ -19,14 +10,46 @@ class Food {
 
 
 
+
+
 class Customer {
     constructor(first_name, order) {
         this.first_name = first_name;
         this.order = order;
     }
 
+    tell_order() {
+        const order_text = document.getElementById('order');
+        let message = 'Can I get ';
+        let add = ' and ';
+        const entries = Array.from(this.order.entries()); // Convert Map to array of entries
+        const lastIndex = entries.length - 1;
+
+        for (let i = 0; i < entries.length; i++) {
+            const [foodItem, quantity] = entries[i];
+            const plural = quantity > 1 ? 's' : '';
+
+            if (i === lastIndex - 1) {
+                add = ' and '; // Change to 'and' for the last two items
+            } else if (i === lastIndex) {
+                add = ''; // No additional 'and' for the last item
+            }
+
+            message += `${quantity} ${foodItem.food_name}${plural}${add} `;
+        }
+
+        order_text.innerHTML = message.trim() + '?';
+    }
+
 }
 
+
+const FOODS = [
+    new Food('muffin', 1.25),
+    new Food('cake', 8.50),
+    new Food('pumpkin pie', 10),
+    new Food('coookie', 2.4)
+]
 
 class Game {
     show_learning_prompt(topic) {
@@ -34,35 +57,56 @@ class Game {
         switch (topic) {
             case 'Fractions':
                 prompt = '';
+                break;
             case 'Divison':
                 prompt = '';
+                break;
             case 'Literacy':
                 prompt = '';
+                break;
             case 'Programming':
                 prompt = '';
+                break;
             default:
                 throw new Error('This is not a valid topic!');
         }
     }
 
-    show_order(customer) {
-        const order_text = document.getElementById('order');
-        let message = 'Can I get ';
 
-        for (let i = 0; i < customer.order.length - 1; i++) {
-            message += `${customer.order[i][0]} ${customer.order[i][1].name} and`;
+
+
+}
+
+
+
+class Main {
+    run() {
+        const num_foods_in_order = 3;
+        let orders = new Map();
+        let ran_quantity = 0;
+        let ran_food = 0;
+
+        for (let i = 0; i < num_foods_in_order; i++) {
+            ran_quantity = Math.floor(Math.random() * 5) + 1;
+            ran_food = Math.floor(Math.random() * FOODS.length);
+
+            const foodItem = FOODS[ran_food];
+
+            if (orders.has(foodItem)) {
+                orders.set(foodItem, orders.get(foodItem) + ran_quantity);
+            } else {
+                orders.set(foodItem, ran_quantity);
+            }
         }
-        order_text.innerHTML = message;
+
+        const customer = new Customer('Joe', orders);
+
+
+        customer.tell_order();
+
     }
 
 
 }
 
-
-
-class main {
-
-
-
-
-}
+new Main().run()
